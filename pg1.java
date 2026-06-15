8bb28b5b724b8a06fe29ada9f33ae2b4227c55e8
HariDharsaun <haridharsaun18personal@gmail.com>
2026-06-15 15:47:21 +0530

    Programme 1 completed successfully

---
+++ pg1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class pg1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+
+        System.out.println("Hello!");
+        System.out.print("I am " + name);
+    }
+}
